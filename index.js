ea19c3c4cbc7283fde383a1aa33ffe69502e2926
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-22 05:46:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+function deleteFromObjectByKey(object, key){
+  delete object[key];
+}
