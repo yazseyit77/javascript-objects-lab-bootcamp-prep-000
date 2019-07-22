6b44f0053696170723affe00f41f7178dfacad8a
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-22 05:42:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var recipes = {
   salad: "tomato"
 };
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
